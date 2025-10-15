f7978ccf5ba53c9a016ced7ce09faec7f491fc89
Seolin Yang (Kirby) <bisor0627@gmail.com>
2025-10-15 18:46:50 +0900

    ✨ Feat. 버스 API 연동 환경 구성
    Why:
    - 버스 공공데이터를 안전하게 호출할 실행 환경이 필요함
    - 테스트 화면에서 다양한 API 응답을 검증해야 함
    
    How:
    - 버스 도메인 모델과 Moya 기반 네트워크 계층을 구축
    - Info.plist와 XCConfig에 서비스 키 주입 구조 및 제공자 추가
    - 버스 API 테스트 UI와 로딩 인디케이터를 구성하고 프로젝트 설정 갱신

---
+++ OffStageApp/Sources/Infrastructure/BusAPI.swift
@@ -0,0 +1,89 @@
+import Foundation
+import Moya
+
+enum BusAPI {
+    // Arrival Info
+    case getArrivals(cityCode: String, nodeId: String)
+    case getArrivalsForRoute(cityCode: String, nodeId: String, routeId: String)
+
+    // Location Info
+    case getRouteBusLocations(cityCode: String, routeId: String)
+
+    // Stop Info
+    case searchStop(cityCode: String, stopName: String)
+    case getStopsByGps(gpsLati: Double, gpsLong: Double)
+    case getStopRoutes(cityCode: String, nodeId: String)
+
+    // Route Info
+    case getRouteInfo(cityCode: String, routeId: String)
+    case searchRoute(cityCode: String, routeNo: String)
+    case getRouteStops(cityCode: String, routeId: String)
+}
+
+extension BusAPI: TargetType {
+    var baseURL: URL {
+        URL(string: "https://apis.data.go.kr/1613000")!
+    }
+
+    var path: String {
+        switch self {
+        case .getArrivals: "/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList"
+        case .getArrivalsForRoute: "/ArvlInfoInqireService/getSttnAcctoSpcifyRouteBusArvlPrearngeInfoList"
+        case .getRouteBusLocations: "/BusLcInfoInqireService/getRouteAcctoBusLcList"
+        case .searchStop: "/BusSttnInfoInqireService/getSttnNoList"
+        case .getStopsByGps: "/BusSttnInfoInqireService/getCrdntPrxmtSttnList"
+        case .getStopRoutes: "/BusSttnInfoInqireService/getSttnThrghRouteList"
+        case .getRouteInfo: "/BusRouteInfoInqireService/getRouteInfoIem"
+        case .searchRoute: "/BusRouteInfoInqireService/getRouteNoList"
+        case .getRouteStops: "/BusRouteInfoInqireService/getRouteAcctoThrghSttnList"
+        }
+    }
+
+    var method: Moya.Method { .get }
+
+    var task: Moya.Task {
+        let serviceKey = APIKeyProvider.busServiceKey
+        var p: [String: Any] = ["serviceKey": serviceKey, "_type": "json"]
+
+        switch self {
+        case let .getArrivals(cityCode, nodeId):
+            p["cityCode"] = cityCode
+            p["nodeId"] = nodeId
+            p["numOfRows"] = 50
+        case let .getArrivalsForRoute(cityCode, nodeId, routeId):
+            p["cityCode"] = cityCode
+            p["nodeId"] = nodeId
+            p["routeId"] = routeId
+        case let .getRouteBusLocations(cityCode, routeId):
+            p["cityCode"] = cityCode
+            p["routeId"] = routeId
+            p["numOfRows"] = 50
+        case let .searchStop(cityCode, stopName):
+            p["cityCode"] = cityCode
+            p["nodeNm"] = stopName
+            p["numOfRows"] = 30
+        case let .getStopsByGps(gpsLati, gpsLong):
+            p["gpsLati"] = gpsLati
+            p["gpsLong"] = gpsLong
+        case let .getStopRoutes(cityCode, nodeId):
+            p["cityCode"] = cityCode
+            p["nodeId"] = nodeId
+            p["numOfRows"] = 50
+        case let .getRouteInfo(cityCode, routeId):
+            p["cityCode"] = cityCode
+            p["routeId"] = routeId
+        case let .searchRoute(cityCode, routeNo):
+            p["cityCode"] = cityCode
+            p["routeNo"] = routeNo
+            p["numOfRows"] = 30
+        case let .getRouteStops(cityCode, routeId):
+            p["cityCode"] = cityCode
+            p["routeId"] = routeId
+            p["numOfRows"] = 100
+        }
+
+        return .requestParameters(parameters: p, encoding: URLEncoding.queryString)
+    }
+
+    var headers: [String: String]? { ["Content-type": "application/json"] }
+}
