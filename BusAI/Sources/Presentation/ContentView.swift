60594ee42f8d36fd3d73adc5f419670c9c0e5e25
Seolin Yang (Kirby) <bisor0627@gmail.com>
2025-10-21 11:32:19 +0900

    Revert "Merge pull request #21 from DeveloperAcademy-POSTECH/feature/1-ai-bus-detect"
    This reverts commit 5b1948683f820297c787b31feb29c92baf55b991, reversing
    changes made to bb73f1b2a68547dca0efda3957e31b90cc9edd8d.

--- BusAI/Sources/Presentation/ContentView.swift
+++ BusAI/Sources/Presentation/ContentView.swift
@@ -1,9 +1,13 @@
+import AVFoundation
 import SwiftUI
+import System
 
 public struct ContentView: View {
+    public init() {}
+
     public var body: some View {
-        BusDetectionView()
-            .ignoresSafeArea()
+        Text("BusAI")
+            .padding()
     }
 }
 
