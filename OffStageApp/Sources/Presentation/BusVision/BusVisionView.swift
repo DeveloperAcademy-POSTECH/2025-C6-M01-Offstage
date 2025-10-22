6e094f4a6a1444170d9a789ffd3f1858a32340cc
SSOL <msseock@gmail.com>
2025-10-22 16:55:16 +0900

    ✨ Feat. 상위뷰에서 버스 노선 번호 전달 기능 추가
    Why:
    - 선택된 버스 노선에 따라 동적으로 정보를 표시하기 위함
    - 상위 화면에서 노선 정보를 전달받아 유연한 화면 구성 필요
    
    How:
    - BusVisionView 초기화 시 노선 번호를 파라미터로 받도록 리팩토링
    - 라우팅 로직 업데이트하여 관련 버스 노선 정보 전달
    - 전달받은 노선 번호를 기반으로 BusVisionView가 동적으로 정보 표시
    
    Tag: #33

--- OffStageApp/Sources/Presentation/BusVision/BusVisionView.swift
+++ OffStageApp/Sources/Presentation/BusVision/BusVisionView.swift
@@ -2,9 +2,15 @@ import SwiftUI
 
 /// router와 연결되는 메인 버스 비전 뷰
 struct BusVisionView: View {
+    // properties
+    var routeNumbers: [String]
+    
     @EnvironmentObject var router: Router<AppRoute>
-    // TODO: Router 수정 후 입력받아 활용 예정. 현재는 1142로 미리 값을 입력해두었습니다.
-    var routeNumbers: [String] = ["1142"]
+
+    // init
+    init(routeNumbers: [String]) {
+        self.routeNumbers = routeNumbers
+    }
 
     var body: some View {
         VStack {
@@ -12,9 +18,4 @@ struct BusVisionView: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         }
     }
-}
-
-#Preview {
-    // 미리보기용 Mock Router
-    RouterView(router: Router<AppRoute>(root: .busvision))
 }
