527b17ab771c26d4f11d79e92b409d3e22a61ff7
SSOL <msseock@gmail.com>
2025-10-23 00:29:14 +0900

    ♻️ Refactor. HomeView 리팩토링 및 뷰로직 추가
    Why:
    - 한 정류소만 활성화되도록 뷰 로직 수정
    - 기존 모델명과 통일성 부여
    - vm 도입을 위한 준비
    
    How:
    - fetchData() 메서드 추가하여 더미 데이터 생성 로직 분리: 추후 뷰모델에서 해당 코드와 같이 데이터 입력하면 뷰를 그릴 수 있도록 처리
    - HomeView에서 알림 활성화 로직 개선 및 추가
    - BusStationCardSubView가 데이터를 직접 생성하지 않고 외부에서 전달받도록 수정
    - BusStationData를 BusStopForHome으로 이름 변경 및 프로퍼티명 명확화
    
    Tag: #38

--- OffStageApp/Sources/Presentation/Home/SubView/BusStationCardSubView.swift
+++ OffStageApp/Sources/Presentation/Home/SubView/BusStationCardSubView.swift
@@ -2,30 +2,44 @@ import SwiftUI
 
 struct BusStationCardSubView: View {
     @EnvironmentObject var router: Router<AppRoute>
-    // 더미파일(실제 데이터 넣을 때는 삭제)
-    let stationSempleItem: BusStationData
-    @State private var isNotificationOn = false
+
+    let busStopData: BusStopForHome
+    let isNotificationOn: Bool
+    let activateNotification: () -> Void
+
+    init(
+        busStopData: BusStopForHome,
+        isNotificationOn: Bool,
+        activateNotification: @escaping () -> Void
+    ) {
+        self.busStopData = busStopData
+        self.isNotificationOn = isNotificationOn
+        self.activateNotification = activateNotification
+    }
 
     var body: some View {
         VStack {
             HStack {
                 VStack(alignment: .leading) {
                     // 정류소 이름 표시
-                    Text("\(stationSempleItem.stationName)")
+                    Text("\(busStopData.nodenm)")
                         .font(.title2)
                     // 정류소 번호 표시
-                    Text("\(stationSempleItem.stationNumber)")
+                    Text("\(busStopData.nodeno)")
                         .foregroundColor(.gray)
                 }
                 Spacer()
                 // 알림 버튼
-                Button(action: {
-                    // 알림 값
-                    isNotificationOn.toggle()
-                }) {
-                    Image(systemName: isNotificationOn ? "bell.circle.fill" : "bell.circle")
-                        .font(.system(size: 40))
-                        .foregroundColor(.gray)
+                Button(action: activateNotification) {
+                    Image(systemName: isNotificationOn ? "bell.fill" : "bell")
+                        .font(.title2)
+                        .foregroundColor(isNotificationOn ? .white : .gray)
+                        .padding(8)
+                        .background {
+                            Circle()
+                                .stroke(lineWidth: 2)
+                                .foregroundStyle(isNotificationOn ? .blue : .gray)
+                        }
                 }
             }
             .padding([.top, .leading, .trailing])
@@ -44,20 +58,31 @@ struct BusStationCardSubView: View {
                 .padding()
             }
 
-            BusRouteListSubView(buses: stationSempleItem.busRoutes)
+            BusRouteListSubView(buses: busStopData.routes)
         }
         .background(.gray.opacity(0.1))
         .cornerRadius(15)
+        .overlay {
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(
+                    isNotificationOn ? .blue : .clear,
+                    lineWidth: 2
+                )
+        }
     }
 }
 
 extension BusStationCardSubView {
     /// busvision에 전달하기 위한 계산 프로퍼티
     var routes: [String] {
-        stationSempleItem.busRoutes.compactMap(\.routeNumber)
+        busStopData.routes.compactMap(\.routeNumber)
     }
 }
 
 #Preview {
-    BusStationCardSubView(stationSempleItem: busStationData[0])
+    BusStationCardSubView(
+        busStopData: busStationData[0],
+        isNotificationOn: true,
+        activateNotification: {}
+    )
 }
